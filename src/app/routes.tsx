675f88f0d590ddbb7d2e855de0541876dd7c3f56
seonghoon07 <chltjdgns1009@gmail.com>
2025-05-05 23:49:23 +0900

    feat(#3) :: 역할 선택 페이지 퍼블리싱

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import StartPage from '@/pages/StartPage';
+import ChooseRole from '@/pages/ChooseRole';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<StartPage />} />
+      <Route path="/role" element={<ChooseRole />} />
     </Routes>
   );
 }
