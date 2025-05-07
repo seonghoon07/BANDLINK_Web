f8c9f58df2ff56c826061a5bb846e9145588badb
seonghoon07 <chltjdgns1009@gmail.com>
2025-05-07 11:50:36 +0900

    feat(#3) :: atom 전역변수 설정

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import StartPage from '@/pages/StartPage';
 import ChooseRole from '@/pages/ChooseRole';
-import InputNickname from '@/pages/InputNickname';
+import SignupInfo from '@/pages/SignupInfo';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<StartPage />} />
       <Route path="/role" element={<ChooseRole />} />
-      <Route path="/nickname" element={<InputNickname />} />
+      <Route path="/nickname" element={<SignupInfo />} />
+      <Route path="/bandname" element={<SignupInfo />} />
     </Routes>
   );
 }
--- src/pages/InputNickname/index.tsx
+++
@@ -1,5 +0,0 @@
-import Content from '@/features/InputNickname/Content';
-
-export default function InputNickname() {
-  return <Content />;
-}
