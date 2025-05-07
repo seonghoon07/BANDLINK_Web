aeb431076225eeca9fa8653498341f72836eee0d
seonghoon07 <chltjdgns1009@gmail.com>
2025-05-07 10:38:10 +0900

    feat(#3) :: 닉네임 입력 페이지 퍼블리싱

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import StartPage from '@/pages/StartPage';
 import ChooseRole from '@/pages/ChooseRole';
+import InputNickname from '@/pages/InputNickname';
 
 export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<StartPage />} />
       <Route path="/role" element={<ChooseRole />} />
+      <Route path="/nickname" element={<InputNickname />} />
     </Routes>
   );
 }
