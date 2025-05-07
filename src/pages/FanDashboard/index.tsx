d169b5cd4a2d2a7270388ebb540de33a40641094
seonghoon07 <chltjdgns1009@gmail.com>
2025-05-07 16:06:59 +0900

    feat(#5) :: Navigation Bar 생성

--- src/pages/FanDashboard/index.tsx
+++ src/pages/FanDashboard/index.tsx
@@ -1,10 +1,14 @@
 import * as S from './style.css';
 import Content from '@/features/FanDashboard/Content';
+import Header from '@/components/Header';
+import NavigationBar from '@/components/NavigationBar';
 
 export default function FanDashboard() {
   return (
     <div className={S.layout}>
+      <Header />
       <Content />
+      <NavigationBar />
     </div>
   );
 }
