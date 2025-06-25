72e45cbf96f159209c227f4ffc09a09323947ab9
seonghoon07 <chltjdgns1009@gmail.com>
2025-06-25 10:22:01 +0900

    feat(#17) :: react-query 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from '@/app/App';
 import { ScrollToTop } from '@/components/layout/ScrollToTop';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <Router>
-    <ScrollToTop />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ScrollToTop />
+      <App />
+    </QueryClientProvider>
   </Router>
 );
---
+++ src/shared/types/userType.ts
@@ -0,0 +1,1 @@
+export type UserType = 'FAN' | 'BAND' | 'PLACE_OWNER';
