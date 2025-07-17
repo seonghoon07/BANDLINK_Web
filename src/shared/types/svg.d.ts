a65b5635e021b0c3a53b46de650d29f87fc1dfdb
seonghoon07 <chltjdgns1009@gmail.com>
2025-07-17 15:02:01 +0900

    feat(#55) :: png 에러 수정

--- src/shared/types/svg.d.ts
+++ src/shared/types/svg.d.ts
@@ -3,3 +3,28 @@ declare module '*.svg?react' {
   const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default ReactComponent;
 }
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.webp' {
+  const value: string;
+  export default value;
+}
